3de04e3eb301524945b49a3dfa29cc93cdbc28d3
JoseAugusto83 <ja834136@gmail.com>
2024-09-30 17:50:18 +0000

    feat: first commit

---
+++ src/main/java/application/controller/CategoriaController.java
@@ -0,0 +1,29 @@
+package application.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import application.model.Categoria;
+import application.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+    @Autowired
+    CategoriaRepository categoriaRepo;
+
+    @GetMapping
+    public Iterable<Categoria> get(){
+        return categoriaRepo.findAll();
+    }
+
+    @PostMapping
+    public Categoria post(@RequestBody Categoria categoria){
+        return categoriaRepo.save(categoria);
+    }
+    
+}
---
+++ src/main/java/application/model/Categoria.java
@@ -0,0 +1,30 @@
+package application.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categorias")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Categoria {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+}
